c5e684b22fdd8b83250d60c4ce8b53b864340a6e
dongbohu <dongbo.hu@gmail.com>
2019-11-21 16:16:37 -0500

    Move deployment script to  section

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Remove original directories built in "static/" dir
+cd ~ubuntu/www/static/
+rm -rf css/ js/ media/
+
+# Copy new files in "build/" directory
 mv /tmp/build/static/* ~ubuntu/www/static/
 rmdir /tmp/build/static/
 mv /tmp/build/* ~ubuntu/www/
