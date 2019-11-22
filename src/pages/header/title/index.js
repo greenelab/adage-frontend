8df788da08f2ac42e8ab30834b516c8afb74852c
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-11-22 13:58:24 -0500

    add version # on hover

--- src/pages/header/title/index.js
+++ src/pages/header/title/index.js
@@ -5,8 +5,10 @@ import { ReactComponent as Logo } from '../../../images/logo-small.svg';
 
 import './index.css';
 
+import packageJson from '../../../../package.json';
+
 const Title = () => (
-  <Link to='/' className='page_header_column'>
+  <Link to='/' className='page_header_column' title={packageJson.version}>
     <Logo className='logo_small' />
     <span className='text_large'>adage</span>
   </Link>
