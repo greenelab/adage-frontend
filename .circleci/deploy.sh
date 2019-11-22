6a58677d3ee49f28a479aeca58c400789c57fd99
dongbohu <dongbo.hu@gmail.com>
2019-11-22 12:07:53 -0500

    Simplify deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Save the Django-REST-Framework related sub-folders in "static/" first
-rm -rf /tmp/drf && mkdir /tmp/drf/
-mv ~ubuntu/www/static/admin/ ~ubuntu/www/static/rest_framework/ /tmp/drf/
-
-# Copy everything in "build/"
-rm -rf ~ubuntu/www/*
-mv /tmp/build/* ~ubuntu/www/
-
-# Move the Django-REST-Framework related sub-folders back to "static/"
-mv /tmp/drf/* ~ubuntu/www/static/
-
-# Clean up
-rmdir /tmp/drf
-rmdir /tmp/build
+# Use "/tmp/build/" to overwrite "/<path>/www/front-end/"
+rm -rf ~ubuntu/www/front-end/
+mv /tmp/build /home/ubuntu/www/front-end
