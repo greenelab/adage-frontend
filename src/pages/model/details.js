bbf0fbfb93e88c8dcffc483ad5070272051d959b
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-11-22 12:50:22 -0500

    section header css, linkify, more

--- src/pages/model/details.js
+++ src/pages/model/details.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import Linkify from 'react-linkify';
 
 import './details.css';
 
 const Details = ({ details }) => (
   <section>
-    {Object.keys(details).map((key) => (
-      <div key={key} className='model_detail_row'>
-        <span className='text_small semibold'>{key}</span>
-        <span>{details[key]}</span>
-      </div>
-    ))}
+    {Object.keys(details)
+      .filter(filter)
+      .map((key) => (
+        <div key={key} className='model_detail_row'>
+          <span className='text_small semibold'>{key}</span>
+          <span>
+            <Linkify>{format(details[key])}</Linkify>
+          </span>
+        </div>
+      ))}
   </section>
 );
 
 export default Details;
+
+const format = (value) => {
+  const type = typeof value;
+  if (type === 'string' || (type === 'number' && !Number.isNaN(value))) {
+    value = String(value);
+    value = value.split('\n').map((line) => <div>{line}</div>);
+  } else
+    value = '-';
+
+  return value;
+};
+
+const filter = (key) => {
+  return key !== 'selected';
+};
