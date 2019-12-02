815bd09c58d849b4cba9883c7c4f50f69506bb27
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-12-02 10:23:38 -0500

    css tweaks, abstract out query reducer (#30)
    - minor css tweaks
    - rearrange some code (minor)
    - make generic query reducer

--- src/reducers/genes.js
+++ src/reducers/genes.js
@@ -1,24 +1,13 @@
 import produce from 'immer';
 
+import { reduceQuery } from './query.js';
+
 const reducer = produce((draft, action) => {
   switch (action.type) {
     case 'SEARCH_GENES_STARTED':
-      draft.geneResults = 'loading';
-      break;
-
     case 'SEARCH_GENES_SUCCEEDED':
-      if (Array.isArray(action.payload)) {
-        if (action.payload.length)
-          draft.geneResults = action.payload;
-        else
-          draft.geneResults = 'empty';
-      } else
-        draft.geneResults = 'error';
-
-      break;
-
     case 'SEARCH_GENES_FAILED':
-      draft.geneResults = 'error';
+      draft.geneResults = reduceQuery(action);
       break;
 
     default:
@@ -29,14 +18,15 @@ const reducer = produce((draft, action) => {
 export default reducer;
 
 export const getGeneResults = (state) => {
-  if (!Array.isArray(state.geneResults))
+  if (Array.isArray(state.geneResults)) {
+    return {
+      genes: state.geneResults.map((gene) => ({
+        standard_name: gene.standard_name,
+        systematic_name: gene.systematic_name,
+        entrezid: gene.entrezid,
+        description: gene.description
+      }))
+    };
+  } else
     return { genes: state.geneResults };
-  return {
-    genes: state.geneResults.map((gene) => ({
-      standard_name: gene.standard_name,
-      systematic_name: gene.systematic_name,
-      entrezid: gene.entrezid,
-      description: gene.description
-    }))
-  };
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,14 @@
 import models from './models.js';
 import genes from './genes.js';
 
-const reducer = (state = {}, action) => compose(models, genes)(state, action);
+const reducer = (state = {}, action = {}) =>
+  compose(models, genes)(state, action);
 
 export default reducer;
 
 // simplifies
-// func1(func2(func3(firstArg, secondArg), secondArg), secondArg)
+// func1(func2(func3(state, action), action), action)
 // to
-// compose(func1, func2, func3)(firstArg, secondArg)
+// compose(func1, func2, func3)(state, action)
 const compose = (...funcs) =>
   funcs.reduce((a, b) => (state, action) => a(b(state, action), action));
