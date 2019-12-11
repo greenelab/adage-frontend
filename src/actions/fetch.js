d585b92b1cebccdfc6ab7a0242b1f8b758f8d565
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-12-11 15:41:03 -0500

    add fetch delay back in

--- src/actions/fetch.js
+++ src/actions/fetch.js
@@ -96,8 +96,8 @@ export const cancelAction = ({ cancelTypeRegex }) => {
 };
 
 function* fetchJson(url, signal) {
-  // artificial random delay for testing loading spinners and race conditions
-  // yield sleep(Math.random() * 1000);
+  // artificial delay for testing loading spinners and race conditions
+  yield sleep(window.sleep || 0);
 
   const cachedResponse = window.sessionStorage.getItem(url);
   if (cachedResponse)
