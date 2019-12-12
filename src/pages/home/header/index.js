d02f69c8102ef49ea43886df011537414f124e8f
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-12-12 10:30:21 -0500

    tweak logo svg animation, add version number to landing pageheader

--- src/pages/home/header/index.js
+++ src/pages/home/header/index.js
@@ -4,8 +4,10 @@ import { ReactComponent as Logo } from '../../../images/logo.svg';
 
 import './index.css';
 
+import packageJson from '../../../../package.json';
+
 const Header = () => (
-  <header className='home_header'>
+  <header className='home_header' title={packageJson.version}>
     <Logo />
     <span className='text_huge'>adage</span>
   </header>
