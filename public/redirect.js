bbc994e78bb4b5b35be2a0a28123ac3cff21a72b
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-01-14 15:22:37 -0500

    change redirect method (#75)

--- public/redirect.js
+++ public/redirect.js
@@ -1,24 +1,11 @@
-// from https://github.com/rafrex/spa-github-pages/blob/gh-pages/index.html
+// based on https://github.com/rafrex/spa-github-pages/blob/gh-pages/index.html
 
-(function(l) {
-  if (l.search) {
-    var q = {};
-    l.search
-      .slice(1)
-      .split('&')
-      .forEach(function(v) {
-        var a = v.split('=');
-        q[a[0]] = a
-          .slice(1)
-          .join('=')
-          .replace(/~and~/g, '&');
-      });
-    if (q.p !== undefined) {
-      window.history.replaceState(
-        null,
-        null,
-        l.pathname.slice(0, -1) + (q.p || '') + (q.q ? '?' + q.q : '') + l.hash
-      );
-    }
-  }
-})(window.location);
+const path = JSON.parse(
+  window.atob(new URLSearchParams(window.location.search).get('path') || '') ||
+    null
+);
+
+if (path) {
+  const url = (path.pathname || '') + (path.search || '');
+  window.history.replace(null, null, url);
+}
