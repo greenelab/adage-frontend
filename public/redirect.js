ef1813f4e424c113386c2d5b0565b5098fe13cb8
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-01-16 12:29:21 -0500

    test

--- public/redirect.js
+++ public/redirect.js
@@ -1,24 +1,11 @@
-// from https://github.com/rafrex/spa-github-pages/blob/gh-pages/index.html
+// based on https://github.com/rafrex/spa-github-pages/blob/gh-pages/index.html
 
-const { search, pathname, hash } = window.location;
+const path =
+  window.decodeURIComponent(
+    new URLSearchParams(window.location.search).get('path') || ''
+  ) || null;
 
-if (search) {
-  const q = {};
-  search
-    .slice(1)
-    .split('&')
-    .forEach((v) => {
-      const a = v.split('=');
-      q[a[0]] = a
-        .slice(1)
-        .join('=')
-        .replace(/~and~/g, '&');
-    });
-  if (q.p !== undefined) {
-    window.history.replaceState(
-      null,
-      null,
-      pathname.slice(0, -1) + (q.p || '') + (q.q ? '?' + q.q : '') + hash
-    );
-  }
-}
+console.log(window.location.href, path);
+
+if (path)
+  window.history.replaceState(null, null, path);
