d9265649d64c24b4efacec31266d95e84577e95d
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-02-18 13:05:47 -0500

    update

--- src/pages/experiments/activities/index.js
+++ src/pages/experiments/activities/index.js
@@ -13,7 +13,7 @@ import { isArray } from '../../../util/types';
 import { uniqueMap } from '../../../util/object';
 
 /* eslint import/no-webpack-loader-syntax: off */
-import worker from 'workerize-loader?inline!./cluster';
+import worker from 'workerize-loader!./cluster';
 
 import './index.css';
 
