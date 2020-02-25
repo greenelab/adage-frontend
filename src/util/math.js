d080a0aa0127f6ab55a165965b4734430db3eb73
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-02-25 11:34:19 -0500

    start adding tests, outsource math to stdlib

--- src/util/math.js
+++ src/util/math.js
@@ -1,40 +1,25 @@
-import jStat from 'jstat';
+// https://stdlib.io/docs/api/v0.0.90/@stdlib/stats/iter/mean
+import array2iterator from '@stdlib/array/to-iterator';
+
+// https://stdlib.io/docs/api/v0.0.90/@stdlib/stats/iter/mean
+import itermean from '@stdlib/stats/iter/mean';
+
+// https://stdlib.io/docs/api/v0.0.90/@stdlib/stats/base/dists/hypergeometric/cdf
+import cdf from '@stdlib/stats/base/dists/hypergeometric/cdf';
+
+// https://stdlib.io/docs/api/v0.0.90/@stdlib/stats/ttest2
+import ttest2 from '@stdlib/stats/ttest2';
+
+// https://github.com/cmpolis/hcluster.js
 import hcluster from 'hclusterjs';
+
+// https://github.com/Planeshifter/multtest
 import { fdr } from 'multtest';
+
 import { isArray } from './types';
-import { isNumber } from './types';
 
 // get mean/average of array
-const mean = (array) => {
-  let count = 0;
-  let total = 0;
-
-  for (const element of array) {
-    if (isNumber(element)) {
-      count++;
-      total += element;
-    }
-  }
-
-  if (count > 0)
-    return total / count;
-  else
-    return 0;
-};
-
-// reference
-// https://bitbucket.org/cgreene/integrator/src/1194f7ae1b11057a4649114e1f55570542acbcde/integrator/static/js/imp.js?
-
-// k - number of successes in sample
-// n - number of items in sample
-const binomialLog = (n, k) => {
-  let coefficient = 0;
-  for (let i = n - k + 1; i <= n; i++)
-    coefficient += Math.log(i);
-  for (let i = 1; i <= k; i++)
-    coefficient -= Math.log(i);
-  return coefficient;
-};
+export const mean = (array) => itermean(array2iterator(array));
 
 // reference
 // https://en.wikipedia.org/wiki/Hypergeometric_distribution
@@ -43,15 +28,10 @@ const binomialLog = (n, k) => {
 // K - number of successes in population
 // n - number of items in sample
 // N - number of items in population
-const hyperGeometricTest = (k, K, n, N) => {
-  let sum = 0;
-  for (let i = 0; i < k; i++) {
-    sum += Math.exp(
-      binomialLog(K, i) + binomialLog(N - K, n - i) - binomialLog(N, n)
-    );
-  }
-  return sum;
-};
+export const hyperGeometricTest = (k, K, n, N) => cdf(k - 1, N, K, n);
+
+// perform two-sample, unpaired, welch's (student's) t-test and return p value
+export const ttest = (array1, array2) => ttest2(array1, array2).pValue;
 
 // calculate enriched signatures
 // adapted from https://github.com/greenelab/adage-server/blob/master/interface/src/app/gene/enriched_signatures.js
@@ -219,31 +199,4 @@ export const calculateVolcanoSignatures = ({
   }));
 
   return volcanoSignatures;
-};
-
-// perform two-sample, unpaired, welch's (student's) t-test and return p value
-// from https://github.com/jstat/jstat/issues/189#issuecomment-469255296
-// note: cannot use the 'ttest' library because downstream dependency is
-// written in web assembly and cannot be compiled by 'workerize-loader' yet
-const ttest = (array1, array2) => {
-  const n1 = array1.length; // array 1 length
-  const n2 = array2.length; // array 2 length
-  const n12 = Math.pow(n1, 2); // raised array 1 length
-  const n22 = Math.pow(n2, 2); // raised array 2 length
-  const meanA = jStat.mean(array1); // mean of array 1
-  const meanB = jStat.mean(array2); // mean of array 2
-  // raised stdev of array 1
-  const stdevA2 = Math.pow(jStat.stdev(array1, true), 2);
-  // raised stdev of array 2
-  const stdevB2 = Math.pow(jStat.stdev(array2, true), 2);
-  // raised by 4 stdev of array 1
-  const stdevA4 = Math.pow(jStat.stdev(array1, true), 4);
-  // raised by 4 stdev of array 2
-  const stdevB4 = Math.pow(jStat.stdev(array2, true), 4);
-  const t = (meanA - meanB) / Math.sqrt(stdevA2 / n1 + stdevB2 / n2);
-  const dfUpper = Math.pow(stdevA2 / n1 + stdevB2 / n2, 2);
-  const dfLower = stdevA4 / (n12 * (n1 - 1)) + stdevB4 / (n22 * (n2 - 1));
-  const df = Math.round(dfUpper / dfLower);
-  const pValue = jStat.studentt.cdf(-Math.abs(t), df) * 2;
-  return pValue;
 };
