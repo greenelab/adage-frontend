c10780289ee60d1ff97ba6084fffa131e98ae524
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-03-09 14:44:39 -0400

    remove vestigial code, tweak appearance

--- src/pages/home/header/index.js
+++ src/pages/home/header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import * as Color from 'color';
 
 import { ReactComponent as AdageLogo } from '../../../images/logo.svg';
 
@@ -35,18 +34,15 @@ export default Header;
 
 const cos = (degrees) => Math.cos((2 * Math.PI * degrees) / 360);
 
-const blend = (colorA, colorB, amount) =>
-  Color(colorA).mix(Color(colorB), amount);
-
 const visualization = (canvas) => {
   // settings
   const resolution = 2; // set to 2x for high dpi screens
   const fps = 50; // frames per second
   const background = '#000000'; // background color
-  const colors = ['#26a36c']; // list of colors from edge to center
+  const color = '#26a36c'; // dot color
   const blur = 50; // canvas blur
   const spacing = 10; // space between waves/rows
-  const size = 1; // dot radius
+  const size = 0.5; // dot radius
   const minSpeed = 1; // dot min horizontal speed
   const maxSpeed = 1; // dot max horizontal speed
   const ampMax = 10; // range of amplitude of wave
@@ -54,7 +50,6 @@ const visualization = (canvas) => {
   const freqMin = 1; // min frequency of wave
   const freqMax = 3; // range of frequency of wave
   const freqFall = 500; // how fast freq falls off away from center
-  const colorFall = 500; // how fast colors fall off away from center
 
   // globals
   const ctx = canvas.getContext('2d');
@@ -93,7 +88,6 @@ const visualization = (canvas) => {
       this.xPrev = undefined;
       this.yPrev = undefined;
       this.speed = undefined;
-      this.color = undefined;
     }
 
     // calculate position and other props
@@ -110,17 +104,11 @@ const visualization = (canvas) => {
       this.speed = Math.sqrt(
         Math.pow(this.x - this.xPrev, 2) + Math.pow(this.y - this.yPrev, 2)
       );
-
-      const colorIndex = Math.pow(1 + 1 / colorFall, -x) * (colors.length - 1);
-      const colorA = colors[Math.floor(colorIndex)];
-      const colorB = colors[Math.ceil(colorIndex)];
-      const amount = colorIndex % 1;
-      this.color = blend(colorA, colorB, amount);
     }
 
     // draw, with interpolated positions based on speed
     draw() {
-      ctx.fillStyle = this.color;
+      ctx.fillStyle = color;
       const steps = this.speed / size;
       for (let step = 0; step < steps; step++) {
         ctx.beginPath();
