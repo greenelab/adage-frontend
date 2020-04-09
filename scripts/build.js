2cada44ed00ba1a3e57d733c73b6aaa328aff288
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-04-09 12:24:07 -0400

    change dir list

--- scripts/build.js
+++ scripts/build.js
@@ -6,6 +6,8 @@ const rewire = require('rewire');
 const defaults = rewire('react-scripts/scripts/build.js');
 const config = defaults.__get__('config');
 
+console.log(process.env);
+
 // consolidate chunk files instead
 config.optimization.splitChunks = {
   cacheGroups: {
