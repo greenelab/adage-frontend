0a794774b2e77c9eeef700db534b9f11a7ad1399
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-04-30 11:42:35 -0400

    update

--- .netlify/plugin/index.js
+++ .netlify/plugin/index.js
@@ -1,12 +1,15 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-// clear cache during build steps
 module.exports = {
   async onInit() {
-    console.log('clearing yarn cache')
+    console.log('deleting yarn.lock');
+    await exec('rm yarn.lock');
+    console.log('deleting node_modules');
+    await exec('rm -rf node_modules');
+    console.log('clearing yarn cache');
     await exec('yarn cache clean');
-    console.log('running yarn install')
+    console.log('running yarn install');
     await exec('yarn install');
-  },
+  }
 };
