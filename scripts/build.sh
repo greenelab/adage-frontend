a670fb040c7f219f3e9db7b483f603b009a9de15
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-04-30 13:06:38 -0400

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,12 @@
+echo "deleting yarn.lock"
 rm -f ./yarn.lock
+echo "creating blank yarn.lock"
+touch ./yarn.lock
+echo "deleting node_modules"
 rm -rf ./node_modules
+echo "clearing yarn cache"
 yarn cache clean
+echo "installing yarn packages"
 yarn install
+echo "running build script"
 node ./scripts/build.js
