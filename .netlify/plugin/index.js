71ef0194ffe2643798b0c05f016264991b125421
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-04-30 11:09:32 -0400

    update

--- .netlify/plugin/index.js
+++ .netlify/plugin/index.js
@@ -1,12 +1,20 @@
 // clear contents of the Netlify build cache before each build
+const clearCache = async ({ utils }) => {
+  await utils.cache.remove('node_modules');
+  const files = await utils.cache.list();
+  console.log(`${files.length} cache files found`);
+  for (const file of files) {
+    console.log(`Deleting cache file ${file}`);
+    await utils.cache.remove(file);
+  }
+};
+
+// clear cache during build steps
 module.exports = {
-  async onPreBuild({ utils }) {
-    await utils.cache.remove('node_modules');
-    const files = await utils.cache.list();
-    console.log(files.length, 'cache files found');
-    for (const file of files) {
-      console.log('Deleting cache file', file);
-      await utils.cache.remove(file);
-    }
+  async onInit({ utils }) {
+    await clearCache({ utils });
+  },
+  async onEnd({ utils }) {
+    await clearCache({ utils });
   }
 };
