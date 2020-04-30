654d5ba5dd3a1c836fe83099909b781bc225eb3d
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-04-30 13:24:25 -0400

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,22 +4,23 @@
 
 
 COLOR='\033[0;31m'
+COLOROFF='\033[0m'
 
-echo -e "${COLOR}Deleting yarn.lock"
+echo -e "${COLOR}=== Deleting yarn.lock ===${COLOROFF}"
 rm -f ./yarn.lock
 
 # if no yarn.lock, Netlify assumes npm and installs packages incorrectly
-echo -e "${COLOR}=== Creating blank yarn.lock ==="
+echo -e "${COLOR}=== Creating blank yarn.lock ===${COLOROFF}"
 touch ./yarn.lock
 
-echo -e "${COLOR}=== Deleting node_modules ==="
+echo -e "${COLOR}=== Deleting node_modules ===${COLOROFF}"
 rm -rf ./node_modules
 
-echo -e "${COLOR}=== Clearing yarn cache ==="
+echo -e "${COLOR}=== Clearing yarn cache ===${COLOROFF}"
 yarn cache clean
 
-echo -e "${COLOR}=== Installing yarn packages ==="
+echo -e "${COLOR}=== Installing yarn packages ===${COLOROFF}"
 yarn install
 
-echo -e "${COLOR}=== Running build script ==="
+echo -e "${COLOR}=== Running build script ===${COLOROFF}"
 node ./scripts/build.js
