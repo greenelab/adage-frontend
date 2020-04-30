f1e154456f80481d4f506f1a62f38aeb69ff23e9
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-04-30 13:17:28 -0400

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,25 @@
-echo "deleting yarn.lock"
+# clear cache and start from blank slate
+# mainly for the benefit of Netlify:
+# https://github.com/greenelab/adage-frontend/issues/130
+
+
+COLOR='\033[0;31m'
+
+echo -e "${COLOR}Deleting yarn.lock"
 rm -f ./yarn.lock
-echo "creating blank yarn.lock"
+
+# if no yarn.lock, Netlify assumes npm and installs packages incorrectly
+echo -e "${COLOR}=== Creating blank yarn.lock ==="
 touch ./yarn.lock
-echo "deleting node_modules"
+
+echo -e "${COLOR}=== Deleting node_modules ==="
 rm -rf ./node_modules
-echo "clearing yarn cache"
+
+echo -e "${COLOR}=== Clearing yarn cache ==="
 yarn cache clean
-echo "installing yarn packages"
+
+echo -e "${COLOR}=== Installing yarn packages ==="
 yarn install
-echo "running build script"
+
+echo -e "${COLOR}=== Running build script ==="
 node ./scripts/build.js
