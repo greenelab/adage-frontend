b2497acddca3dff3dbbb4f4977e9b129aa594bb0
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-05-13 16:11:25 -0400

    add volcano cutoff

---
+++ src/pages/experiments/volcano/filters/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Input from '../../../../components/input';
+import NumberBox from '../../../../components/number-box';
+
+import './index.css';
+
+// filter controls above gene network graph
+
+const Filters = ({ setSearch, setPValueCutoff }) => (
+  <div id='volcano_filters'>
+    <div>
+      <Input
+        className='volcano_search'
+        placeholder='search signature names'
+        onChange={setSearch}
+      />
+    </div>
+    <div>
+      <span className='volcano_cutoff_label'>p Value cutoff:</span>
+      <NumberBox
+        className='volcano_cutoff'
+        defaultValue={1}
+        onChange={setPValueCutoff}
+      />
+    </div>
+  </div>
+);
+
+export default Filters;
