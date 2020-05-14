511fec9517c81e61a64acac94671fda2167bd1b4
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-05-14 13:24:26 -0400

    add epsilon to p values

--- src/util/math.js
+++ src/util/math.js
@@ -7,6 +7,9 @@ import itermean from '@stdlib/stats/iter/mean';
 // https://stdlib.io/docs/api/v0.0.90/@stdlib/stats/base/dists/hypergeometric/cdf
 import cdf from '@stdlib/stats/base/dists/hypergeometric/cdf';
 
+// https://stdlib.io/docs/api/v0.0.90/@stdlib/constants/math/float16-eps
+import epsilon from '@stdlib/constants/math/float64-eps';
+
 // https://stdlib.io/docs/api/v0.0.90/@stdlib/stats/ttest2
 import ttest2 from '@stdlib/stats/ttest2';
 
@@ -319,3 +322,5 @@ export const calculateEnrichedGenes = ({
 
   return enrichedGeneSets;
 };
+
+export { epsilon };
