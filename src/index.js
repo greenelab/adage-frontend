451718fa88d58a48c0b58dabca039e16720091e3
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-09-22 21:32:38 -0400

    add flag for production or development

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import App from './app';
 
 Sentry.init({
   dsn:
-    'https://016df0e3771549f982401f272df8b7e4@o7983.ingest.sentry.io/5438025',
+    'https://016df0e3771549f982401f272df8b7e4@o7983.ingest.sentry.io/5438025', // api key
+  environment: process.env.NODE_ENV, // production or development
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 1.0
 });
