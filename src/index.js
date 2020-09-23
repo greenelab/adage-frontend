dd9c4b2fcb94dde5fbef0397b5935516ba411861
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-09-22 20:16:33 -0400

    add sentry error tracking

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
 import App from './app';
+
+Sentry.init({
+  dsn:
+    'https://016df0e3771549f982401f272df8b7e4@o7983.ingest.sentry.io/5438025',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0
+});
 
 render(<App />, document.getElementById('root'));
