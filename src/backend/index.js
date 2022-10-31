699b2e5b5be882925a7cdab5ac66bc8bdb7d1d85
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-10-31 11:00:58 -0600

    Points frontend at mousiplier API

--- src/backend/index.js
+++ src/backend/index.js
@@ -4,7 +4,7 @@
 // export const server = 'http://127.0.0.1:8000/api/v1/';
 
 // production server
-export const server = 'https://api-adage.greenelab.com/api/v1/';
+export const server = 'https://api-mousiplier.greenelab.com/api/v1/';
 
 // default result limit
 export const defaultLimit = 100;
